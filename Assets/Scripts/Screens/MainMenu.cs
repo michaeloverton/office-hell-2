624054a035afb40fdfb832cb07562e36e1d8c58e
Michael Brown <m.overtonbrown@gmail.com>
2021-08-13 18:36:40 -0700

    modify room colliders and add sounds

--- Assets/Scripts/Screens/MainMenu.cs
+++ Assets/Scripts/Screens/MainMenu.cs
@@ -5,7 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public AudioSource playSound;
+
     public void PlayGame() {
+        StartCoroutine (loadScene());
+    }
+
+    IEnumerator loadScene() {
+        playSound.Play();
+        yield return new WaitWhile (()=> playSound.isPlaying);
+        yield return new WaitForSeconds(0.5f);
+
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
